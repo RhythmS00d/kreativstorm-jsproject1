775ef6c6d37fda99384fef0ba027ba0f07c19a75
RhythmS00d <rhythmsood1@gmail.com>
2024-02-08 12:32:33 +1100

    feat: added global variables and values

---
+++ script.js
@@ -0,0 +1,19 @@
+const introduction =
+  "Welcome, brave coder! You've entered the realm of CodeNemesis, the mischievous AI seeking dominance through Rock, Paper, Scissors. Are you ready for an epic coding adventure?";
+const victoryMessage =
+  "Victory is yours! CodeNemesis has been vanquished, and the coding kingdom is saved!";
+const defeatMessage =
+  "Oh no! CodeNemesis prevails this game. Keep coding, and try again!";
+
+const choices = ["rock", "paper", "scissors"];
+const winConditions = {
+  // playerSelection:computerSelection
+  paper: "rock",
+  scissors: "paper",
+  rock: "scissors",
+};
+
+const scores = {
+  player: 0,
+  computer: 0,
+};
